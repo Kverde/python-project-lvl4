12b8a9b1cc32f20f9772d85f5c4fb2b2a1e0a501
Kverde <kverde@yandex.ru>
2022-05-26 12:50:59 +0500

    tesdf

--- utils/run.sh
+++ utils/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python manage.py collectstatic --noinput
-python manage.py migrate
-gunicorn task_manager.wsgi --bind=0.0.0.0:80
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py migrate
+poetry run gunicorn task_manager.wsgi --bind=0.0.0.0:80
