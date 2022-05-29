8d590cfa5804efee75721b483e1895acf69866b7
Kverde <kverde@yandex.ru>
2022-05-29 14:10:52 +0500

    step 2

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ django_shell:
 	poetry run python manage.py shell_plus
 
 develop:
+	poetry run python manage.py collectstatic --noinput
 	poetry run python manage.py runserver
 
 prod:
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager'
+    'task_manager',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
